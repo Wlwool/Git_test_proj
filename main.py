64b99f4c477e81a8c808bf130dd9b3761aa9e79e
Wlwool <blackenedima@yandex.ru>
2025-04-16 17:07:27 +0300

    .gitignore

--- main.py
+++ main.py
@@ -4,3 +4,20 @@
 # git branch -M main
 # git remote add origin https://github.com/Wlwool/Git_test_proj.git
 # git push -u origin main
+
+class ListWrapper(list):
+    def __init__(self, iterable):
+        self._list = list(iterable)
+
+    def __getitem__(self, index):
+        return self._list[index]
+
+    def __len__(self):
+        return len(self._list)
+
+    def __repr__(self):
+        return f"ListWrapper (length: {len(self)}, elements: {self._list})"
+
+wrapper = ListWrapper([1, 2, 3])
+print(wrapper)          # Вывод: ListWrapper (length: 3, elements: [1, 2, 3])
+print(wrapper[0])
