343a22ce47d449ffe0c28a6f9e9445b68db874ac
Wlwool <blackenedima@yandex.ru>
2025-04-17 13:36:19 +0300

    WIP: незавершённые изменения перед rebase

--- OOP/stack.py
+++ OOP/stack.py
@@ -19,6 +19,10 @@ class Stack:
             raise StackEmptyError("Стек пуст!")
         return self.stack.pop()
 
+    def __len__(self):
+        return len(self.stack)
+
+
     def __repr__(self):
         return f"Stack<size={len(self.stack)}>"
 
@@ -27,16 +31,16 @@ class StackEmptyError(Exception):
     pass
 
 stack = Stack()
-stack.push(1)
-print(stack)
-stack.pop()
-print(stack)
 
-stack = Stack()
 print(stack)
 stack.push(1)
 print(stack)
 stack.push(2)
+stack.push(3)
 print(stack)
 print(stack.pop())
 print(stack)
+print(Stack.__len__(stack))
+
+
+
